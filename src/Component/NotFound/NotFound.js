54b362481401adfb5cec426940494cb879900aef
Khushbu Islam Misbah <khushbuislamoffice@gmail.com>
2021-03-19 19:57:19 +0600

    React-Authentication Assignment-9

---
+++ src/Component/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className="notFound">
+            <h1>Sorry! Your Page Not Found.</h1>
+            <h3>404 ERROR!!!</h3>
+        </div>
+    );
+};
+
+export default NotFound;
